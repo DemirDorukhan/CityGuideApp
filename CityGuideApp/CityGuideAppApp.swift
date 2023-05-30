a8a2a36d23dffed311c908c4eb62fbd433de0591
DemirDorukhan <demirdorukhan@icloud.com>
2023-05-30 13:35:16 +0300

    Initial Commit

---
+++ CityGuideApp/CityGuideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityGuideAppApp.swift
+//  CityGuideApp
+//
+//  Created by Dorukhan Demir on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CityGuideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
